c47ed0afc00ed211097de16130ffddf6dde1898a
mooyaga2 <mooyaga2@linux-qndp.suse>
2016-12-06 12:35:29 +0100

    ...

--- fruit_project.js
+++ fruit_project.js
@@ -1,4 +1,7 @@
-//var serviceRegistry = new orion.ServiceRegistry();
+var serviceRegistry = new orion.serviceRegistry.ServiceRegistry();
+var fileClient = serviceRegistry.getService("orion.core.file.client");
+
+
 var headers = { name: "Fruit Project Plugin", version: "1.0", description: "Plugin providing support for Fruit projects." };
 var provider = new orion.PluginProvider(headers);
 
