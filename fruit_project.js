0f6a536ce076cd1fae7937b335662692ed2a00e5
mooyaga2 <mooyaga2@linux-qndp.suse>
2016-12-06 17:43:13 +0100

    ...

--- fruit_project.js
+++ fruit_project.js
@@ -1,6 +1,6 @@
-define(["orion/plugin", "orion/serviceregistry", "orion/fileClient"], 
+define(["orion/plugin", "orion/serviceregistry", "orion/fileClient", "orion/xhr"], 
        
-       function(PluginProvider, mServiceRegistry, mFileClient) {
+       function(PluginProvider, mServiceRegistry, mFileClient, xhr) {
            var serviceRegistry = new mServiceRegistry.ServiceRegistry();
            
            var headers = { name: "Fruit Project Plugin", version: "1.0", description: "Plugin providing support for Fruit projects." };
@@ -13,37 +13,46 @@ define(["orion/plugin", "orion/serviceregistry", "orion/fileClient"],
                },
                
                initProject: function(params, projectMetadata){
-                   var fileServices = serviceRegistry.getServiceReferences("orion.core.file");
-                   var leSvc = fileServices[0];
-                   // drugi parametar je 'filter'...
-                   var fileClient = new mFileClient.FileClient(serviceRegistry, null);
-                   var url = removeUserInformation(params.url);
-                   fileClient.createProject("/workspace/vekaz-OrionContent", params.url/*, null, true*/);
-                   
-                   return {
-                       ContentLocation: "/workspace/vekaz-OrionContent/" + params.url //projectMetadata.WorkspaceLocation
+                   var request = xhr("GET","/workspace").then(
+                       function(result){
+                           console.log(result);                           
+                       }
                        
-                   }
+                   ) );
+           
+           /*
+            *                    var fileServices = serviceRegistry.getServiceReferences("orion.core.file");
+            *                   var leSvc = fileServices[0];
+            *                   // drugi parametar je 'filter'...
+            *                   var fileClient = new mFileClient.FileClient(serviceRegistry, null);
+            *                   var url = removeUserInformation(params.url);
+            *                   fileClient.createProject("/workspace/vekaz-OrionContent", params.url, null, true);
+            */
+           return {
+               ContentLocation: "/workspace/vekaz-OrionContent/" + params.url //projectMetadata.WorkspaceLocation
+               
+           }
                }
            },
-           {
-               id: "orion.fruit.projecthandler",
-               type: "fruit",
-               addParameters: [{id: "url", type: "text", name: "URL"}],
-               addProjectName: "fruit",
-               addProjectTooltip: "..create a new fruit project.."
-           });                 
-           
-           function removeUserInformation(url){
-               if(url && url.indexOf("@")>0 && url.indexOf("ssh://")>=0){
-                   return url.substring(0, url.indexOf("ssh://") + 6) + url.substring(url.indexOf("@")+1);
-               }
-               return url;
-           }
-           
-           
-           provider.connect();    
+       {
+           id: "orion.fruit.projecthandler",
+       type: "fruit",
+       addParameters: [{id: "url", type: "text", name: "URL"}],
+       addProjectName: "fruit",
+       addProjectTooltip: "..create a new fruit project.."
+       });                 
+
+function removeUserInformation(url){
+    if(url && url.indexOf("@")>0 && url.indexOf("ssh://")>=0){
+        return url.substring(0, url.indexOf("ssh://") + 6) + url.substring(url.indexOf("@")+1);
+    }
+    return url;
+}
+
+
+provider.connect();    
        });
-
-
-
+       
+       
+       
+       
