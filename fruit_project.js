70bcc6fb5f994097b1522054f6413816b9c58dfa
mooyaga2 <mooyaga2@linux-qndp.suse>
2016-12-06 11:20:39 +0100

    ...

--- fruit_project.js
+++ fruit_project.js
@@ -1,3 +1,33 @@
+var serviceRegistry = new orion.serviceRegistry();
 var headers = { name: "Fruit Project Plugin", version: "1.0", description: "Plugin providing support for Fruit projects." };
 var provider = new orion.PluginProvider(headers);
-provider.connect(); 
+
+
+provider.registerService("orion.project.handler", {
+    paramsToDependencyDescription: function(params){
+		return {Type: "fruit", Location: removeUserInformation(params.url)};
+	},
+   /* 
+    initDependency: function(dependency, params, projectMetadata){
+		var url = removeUserInformation(dependency.Location || params.url);
+		return this._cloneRepository(url, params, projectMetadata.WorkspaceLocation);
+    },
+	initProject: function(params, projectMetadata){
+		var url = removeUserInformation(params.url);
+		return this._cloneRepository(url, params, projectMetadata.WorkspaceLocation, true);
+	},
+    */
+});
+        
+function removeUserInformation(url){
+	if(url.indexOf("@")>0 && url.indexOf("ssh://")>=0){
+		return url.substring(0, url.indexOf("ssh://") + 6) + url.substring(url.indexOf("@")+1);
+	}
+	return url;
+}
+        
+        
+        
+        
+        
+provider.connect();
