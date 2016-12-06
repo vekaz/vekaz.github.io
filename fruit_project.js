93b0414c87d89fb598b05da56c66973560a78d46
mooyaga2 <mooyaga2@linux-qndp.suse>
2016-12-06 17:45:19 +0100

    ...

--- fruit_project.js
+++ fruit_project.js
@@ -18,41 +18,40 @@ define(["orion/plugin", "orion/serviceregistry", "orion/fileClient", "orion/xhr"
                            console.log(result);                           
                        }
                        
-                   ) );
-           
-           /*
-            *                    var fileServices = serviceRegistry.getServiceReferences("orion.core.file");
-            *                   var leSvc = fileServices[0];
-            *                   // drugi parametar je 'filter'...
-            *                   var fileClient = new mFileClient.FileClient(serviceRegistry, null);
-            *                   var url = removeUserInformation(params.url);
-            *                   fileClient.createProject("/workspace/vekaz-OrionContent", params.url, null, true);
-            */
-           return {
-               ContentLocation: "/workspace/vekaz-OrionContent/" + params.url //projectMetadata.WorkspaceLocation
-               
-           }
+                   )
+                   
+                   /*
+                    *                    var fileServices = serviceRegistry.getServiceReferences("orion.core.file");
+                    *                   var leSvc = fileServices[0];
+                    *                   // drugi parametar je 'filter'...
+                    *                   var fileClient = new mFileClient.FileClient(serviceRegistry, null);
+                    *                   var url = removeUserInformation(params.url);
+                    *                   fileClient.createProject("/workspace/vekaz-OrionContent", params.url, null, true);
+                    */
+                   return {
+                       ContentLocation: "/workspace/vekaz-OrionContent/" + params.url //projectMetadata.WorkspaceLocation
+                       
+                   }
                }
            },
-       {
-           id: "orion.fruit.projecthandler",
-       type: "fruit",
-       addParameters: [{id: "url", type: "text", name: "URL"}],
-       addProjectName: "fruit",
-       addProjectTooltip: "..create a new fruit project.."
-       });                 
-
-function removeUserInformation(url){
-    if(url && url.indexOf("@")>0 && url.indexOf("ssh://")>=0){
-        return url.substring(0, url.indexOf("ssh://") + 6) + url.substring(url.indexOf("@")+1);
-    }
-    return url;
-}
-
-
-provider.connect();    
+           {
+               id: "orion.fruit.projecthandler",
+               type: "fruit",
+               addParameters: [{id: "url", type: "text", name: "URL"}],
+               addProjectName: "fruit",
+               addProjectTooltip: "..create a new fruit project.."
+           });                 
+           
+           function removeUserInformation(url){
+               if(url && url.indexOf("@")>0 && url.indexOf("ssh://")>=0){
+                   return url.substring(0, url.indexOf("ssh://") + 6) + url.substring(url.indexOf("@")+1);
+               }
+               return url;
+           }
+           
+           
+           provider.connect();    
        });
-       
-       
-       
-       
+
+
+
