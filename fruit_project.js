27f7895bcc3b020ba3a6df05000b9172589c5772
mooyaga2 <mooyaga2@linux-qndp.suse>
2016-12-06 10:51:29 +0100

    ...

--- fruit_project.js
+++ fruit_project.js
@@ -1,3 +1,10 @@
-var headers = { name: "Fruit Project Plugin", version: "1.0", description: "Plugin providing support for Fruit projects." };
-var provider = new orion.PluginProvider(headers);
-provider.connect(); 
+define([
+	"orion/plugin"
+    ], 
+    
+    function(PluginProvider){
+        var headers = { name: "Fruit Project Plugin", version: "1.0", description: "Plugin providing support for Fruit projects." };
+        var provider = new PluginProvider(headers);
+        provider.connect();         
+    }
+);
